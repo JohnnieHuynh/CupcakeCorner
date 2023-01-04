dc390d92bd820821eeaf5218e7386bea57c65357
JohnnieHuynh <huy.j.huynh@gmail.com>
2023-01-04 13:13:26 -0800

    initial commit

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Johnny Huynh on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
